5bdfaae9e811cfef41a3063b6b10ec700b80eac6
kyonsheeee <kyokoakatsuka38@gmail.com>
2023-07-23 00:25:57 +0900

    計算アプリ実装

--- src/index.js
+++ src/index.js
@@ -1,17 +1,68 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class ValueInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  handleChange(e) {
+    this.props.valueChange(e.target.value);
+  }
+
+  render() {
+    const newValue = this.props.value;
+    return <input type="text" value={newValue} onChange={this.handleChange} />;
+  }
+}
+
+class ValueAnswer extends React.Component {
+  render() {
+    return <span>{this.props.value}</span>;
+  }
+}
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.changeValue1 = this.changeValue1.bind(this);
+    this.changeValue2 = this.changeValue2.bind(this);
+    this.state = {
+      value1: 0,
+      value2: 0,
+      answer: 0,
+    };
+  }
+
+  changeValue1(newValue) {
+    let ans = parseInt(newValue) + parseInt(this.state.value2);
+    this.setState({
+      value1: newValue,
+      answer: ans,
+    });
+  }
+
+  changeValue2(newValue) {
+    let ans = parseInt(newValue) + parseInt(this.state.value1);
+    this.setState({
+      value2: newValue,
+      answer: ans,
+    });
+  }
+
+  render() {
+    const newValue1 = this.state.value1;
+    const newValue2 = this.state.value2;
+    return (
+      <div>
+        <ValueInput value={newValue1} valueChange={this.changeValue1} /> +
+        <ValueInput value={newValue2} valueChange={this.changeValue2} /> =
+        <ValueAnswer value={this.state.answer} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Calculator />, document.getElementById("root"));
